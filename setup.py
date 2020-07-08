e396c6ebe95360652806879c36d464fdbc9ed004
Alex <al.orzh@gmail.com>
2020-07-08 12:59:34 +0300

    upadte gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='Cython temp proj app',
     ext_modules=cythonize(
         ["package_b/fib.pyx", "package_b/simple.pyx", "package_a/main.py"],
-        annotate=True,
+        annotate=False,
         compiler_directives={'language_level': "3"}
     ),
     zip_safe=True,
